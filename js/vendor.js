dc8f6e98c9d181866623f939d37b3167b8ad71ee
Julia Apreleva <12630m@mail.ru>
2020-10-14 23:19:49 +0300

    Updates

--- js/vendor.js
+++ js/vendor.js
@@ -3044,8 +3044,7 @@
         module.l = !0, module.exports;
     }
     var modules, installedModules;
-
-    Inputmask({"mask": "8(999)999-99-99"}).mask(phoneMask);
 });
 
+Inputmask({"mask": "8(999)999-99-99"}).mask(phoneMask);
 
